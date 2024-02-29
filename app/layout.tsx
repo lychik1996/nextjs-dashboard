939cedb58ab19faed7b7379c36aa153587468495
Vitaliy Khatsey <vitaliy.khatsey@gmail.com>
2024-02-29 10:46:15 +0200

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
@@ -8,6 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Acme Dashboard</title>
+        <meta name="description" content="The official Next.js build with app Route," />
+      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
